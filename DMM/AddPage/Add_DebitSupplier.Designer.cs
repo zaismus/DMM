1f3eff5515a1bb5e14ca46da5deb4b32542d9fd4
zaismus <63042537+zaismus@users.noreply.github.com>
2022-01-04 09:20:11 +0100

    E20

---
+++ DMM/AddPage/Add_DebitSupplier.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace DMM.AddPage
+{
+    partial class Add_DebitSupplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_DebitSupplier));
+            this.label1 = new System.Windows.Forms.Label();
+            this.edt_debit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_addclose = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_add = new DevExpress.XtraEditors.SimpleButton();
+            this.toastNotificationsManager1 = new DevExpress.XtraBars.ToastNotifications.ToastNotificationsManager(this.components);
+            this.edt_name = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.toastNotificationsManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cairo", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ajouts des dettes";
+            // 
+            // edt_debit
+            // 
+            this.edt_debit.Font = new System.Drawing.Font("Cairo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_debit.Location = new System.Drawing.Point(36, 294);
+            this.edt_debit.Name = "edt_debit";
+            this.edt_debit.Size = new System.Drawing.Size(538, 37);
+            this.edt_debit.TabIndex = 2;
+            this.edt_debit.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cairo", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Le Montant Totale";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.btn_addclose);
+            this.panel1.Controls.Add(this.btn_add);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 362);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 80);
+            this.panel1.TabIndex = 6;
+            // 
+            // btn_addclose
+            // 
+            this.btn_addclose.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btn_addclose.Appearance.Font = new System.Drawing.Font("Cairo", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_addclose.Appearance.Options.UseFont = true;
+            this.btn_addclose.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btn_addclose.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_addclose.ImageOptions.SvgImage")));
+            this.btn_addclose.Location = new System.Drawing.Point(436, 12);
+            this.btn_addclose.Name = "btn_addclose";
+            this.btn_addclose.Size = new System.Drawing.Size(167, 56);
+            this.btn_addclose.TabIndex = 5;
+            this.btn_addclose.Text = "Ajouter+Fermé";
+            this.btn_addclose.Click += new System.EventHandler(this.btn_addclose_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btn_add.Appearance.Font = new System.Drawing.Font("Cairo", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Appearance.Options.UseFont = true;
+            this.btn_add.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btn_add.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_add.ImageOptions.SvgImage")));
+            this.btn_add.Location = new System.Drawing.Point(12, 12);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(167, 56);
+            this.btn_add.TabIndex = 4;
+            this.btn_add.Text = "Ajouter";
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // toastNotificationsManager1
+            // 
+            this.toastNotificationsManager1.ApplicationId = "1f97f2dd-6818-4cc6-b95e-1e88f8893d99";
+            this.toastNotificationsManager1.Notifications.AddRange(new DevExpress.XtraBars.ToastNotifications.IToastNotificationProperties[] {
+            new DevExpress.XtraBars.ToastNotifications.ToastNotification("c5735894-7960-49f3-a6b9-8a4d4ab688fd", ((System.Drawing.Image)(resources.GetObject("toastNotificationsManager1.Notifications"))), "Ajouté avec succès", "Ajouté avec succès", "Ajouté avec succès", DevExpress.XtraBars.ToastNotifications.ToastNotificationTemplate.ImageAndText01),
+            new DevExpress.XtraBars.ToastNotifications.ToastNotification("2119f7a4-b0f2-4ee8-9aea-c107924c374b", ((System.Drawing.Image)(resources.GetObject("toastNotificationsManager1.Notifications1"))), "Les données ont été modifiées avec succès", "Les données ont été modifiées avec succès", "Les données ont été modifiées avec succès", DevExpress.XtraBars.ToastNotifications.ToastNotificationTemplate.ImageAndText01)});
+            // 
+            // edt_name
+            // 
+            this.edt_name.Font = new System.Drawing.Font("Cairo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_name.Location = new System.Drawing.Point(37, 80);
+            this.edt_name.Name = "edt_name";
+            this.edt_name.Size = new System.Drawing.Size(537, 144);
+            this.edt_name.TabIndex = 7;
+            this.edt_name.Text = "";
+            // 
+            // Add_DebitSupplier
+            // 
+            this.Appearance.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 442);
+            this.Controls.Add(this.edt_name);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.edt_debit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IconOptions.ShowIcon = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Add_DebitSupplier";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ajouter un Fournisseur";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.toastNotificationsManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.TextBox edt_debit;
+        public DevExpress.XtraEditors.SimpleButton btn_addclose;
+        public DevExpress.XtraEditors.SimpleButton btn_add;
+        private DevExpress.XtraBars.ToastNotifications.ToastNotificationsManager toastNotificationsManager1;
+        public System.Windows.Forms.RichTextBox edt_name;
+    }
+}
---
+++ DMM/AddPage/Add_DebitSupplier.cs
@@ -0,0 +1,131 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity.Migrations;
+
+namespace DMM.AddPage
+{
+    public partial class Add_DebitSupplier : DevExpress.XtraEditors.XtraForm
+    {
+        public Add_DebitSupplier()
+        {
+            InitializeComponent();
+        }
+
+
+        // Define Database, Table, Page & Var
+        DBDMMEntities db;
+        Debit_Suppliers tbAdd;
+        public DMM.Pages.Page_Suppliers page;
+        public int id;
+        public int SupplierID;
+        public string SupplierName;
+
+
+        // Function Add
+        private void Add()
+        {
+            // Check Empty value
+            if (edt_name.Text == "")
+            {
+                MessageBox.Show("Certains champs sont obligatoires", "", MessageBoxButtons.OK, MessageBoxIcon.Warning); ;
+            }
+            else
+            {
+                // Check if Add or Edit Data
+                if (id == 0)
+                {
+                    AddData();
+                    ClearData();
+                }
+                else
+                {
+                    EditData();
+                }
+
+                // Update Data
+                page.LoadData();
+            }
+        }
+
+        // Add Data
+        private void AddData()
+        {
+            try
+            {
+                db = new DBDMMEntities();
+                tbAdd = new Debit_Suppliers
+                {
+                    FullName = edt_name.Text,
+                    Debit = Convert.ToDouble(edt_debit),
+                    SupplierName = SupplierName,
+                    ID_Supplier = SupplierID,
+                    DateT = DateTime.Now
+                };
+
+                db.Entry(tbAdd).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+                toastNotificationsManager1.ShowNotification("c5735894-7960-49f3-a6b9-8a4d4ab688fd");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Edit Data
+        private void EditData()
+        {
+            try
+            {
+                db = new DBDMMEntities();
+                tbAdd = new Debit_Suppliers
+                {
+                    ID = id,
+                    FullName = edt_name.Text,
+                    Debit = Convert.ToDouble(edt_debit),
+                    SupplierName = SupplierName,
+                    ID_Supplier = SupplierID,
+                    DateT = DateTime.Now
+                };
+
+                db.Set<Debit_Suppliers>().AddOrUpdate(tbAdd);
+                db.SaveChanges();
+                toastNotificationsManager1.ShowNotification("2119f7a4-b0f2-4ee8-9aea-c107924c374b");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            Add();
+        }
+
+        private void btn_addclose_Click(object sender, EventArgs e)
+        {
+            Add();
+            Close();
+        }
+
+        // Clear Value
+        private void ClearData()
+        {
+            edt_name.Text = "";
+            edt_debit.Text = "0";
+        }
+    }
+}
