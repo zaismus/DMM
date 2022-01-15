6f3390d087ac4d62c7117f6f41e585e315f476e1
zaismus <zaismus@gmail.com>
2022-01-15 11:15:49 +0100

    E41

--- DMM/Pages/AnalysisPage.Designer.cs
+++ DMM/Pages/AnalysisPage.Designer.cs
@@ -29,6 +29,8 @@ namespace DMM.Pages
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
@@ -45,6 +47,8 @@ namespace DMM.Pages
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -61,7 +65,7 @@ namespace DMM.Pages
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(15);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1151, 463);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1031, 463);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // panel3
@@ -72,17 +76,17 @@ namespace DMM.Pages
             this.panel3.Location = new System.Drawing.Point(30, 30);
             this.panel3.Margin = new System.Windows.Forms.Padding(15);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(325, 184);
+            this.panel3.Size = new System.Drawing.Size(300, 184);
             this.panel3.TabIndex = 2;
             // 
             // label5
             // 
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Cairo", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Roboto Condensed", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.label5.Location = new System.Drawing.Point(3, 25);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(320, 61);
+            this.label5.Size = new System.Drawing.Size(294, 61);
             this.label5.TabIndex = 1;
             this.label5.Text = "Dettes Fournisseurs";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -92,9 +96,9 @@ namespace DMM.Pages
             this.txt_Supplier.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txt_Supplier.BackColor = System.Drawing.Color.Teal;
             this.txt_Supplier.ForeColor = System.Drawing.Color.White;
-            this.txt_Supplier.Location = new System.Drawing.Point(-24, 115);
+            this.txt_Supplier.Location = new System.Drawing.Point(3, 115);
             this.txt_Supplier.Name = "txt_Supplier";
-            this.txt_Supplier.Size = new System.Drawing.Size(370, 69);
+            this.txt_Supplier.Size = new System.Drawing.Size(300, 69);
             this.txt_Supplier.TabIndex = 0;
             this.txt_Supplier.Text = "1 000 000";
             this.txt_Supplier.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -104,20 +108,20 @@ namespace DMM.Pages
             this.panel2.BackColor = System.Drawing.SystemColors.Control;
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.txt_Customer);
-            this.panel2.Location = new System.Drawing.Point(385, 30);
+            this.panel2.Location = new System.Drawing.Point(360, 30);
             this.panel2.Margin = new System.Windows.Forms.Padding(15);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(325, 184);
+            this.panel2.Size = new System.Drawing.Size(300, 184);
             this.panel2.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Cairo", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Roboto Condensed", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.label3.Location = new System.Drawing.Point(3, 25);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(320, 61);
+            this.label3.Size = new System.Drawing.Size(294, 61);
             this.label3.TabIndex = 1;
             this.label3.Text = "Créances Clients";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,9 +131,9 @@ namespace DMM.Pages
             this.txt_Customer.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txt_Customer.BackColor = System.Drawing.Color.Maroon;
             this.txt_Customer.ForeColor = System.Drawing.Color.White;
-            this.txt_Customer.Location = new System.Drawing.Point(-23, 115);
+            this.txt_Customer.Location = new System.Drawing.Point(3, 115);
             this.txt_Customer.Name = "txt_Customer";
-            this.txt_Customer.Size = new System.Drawing.Size(370, 69);
+            this.txt_Customer.Size = new System.Drawing.Size(300, 69);
             this.txt_Customer.TabIndex = 0;
             this.txt_Customer.Text = "1 000 000";
             this.txt_Customer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -139,20 +143,20 @@ namespace DMM.Pages
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.txt_totalDebit);
-            this.panel1.Location = new System.Drawing.Point(740, 30);
+            this.panel1.Location = new System.Drawing.Point(690, 30);
             this.panel1.Margin = new System.Windows.Forms.Padding(15);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(325, 184);
+            this.panel1.Size = new System.Drawing.Size(300, 184);
             this.panel1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Cairo", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Roboto Condensed", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.label2.Location = new System.Drawing.Point(3, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(320, 61);
+            this.label2.Size = new System.Drawing.Size(294, 61);
             this.label2.TabIndex = 1;
             this.label2.Text = "Dettes totales";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -162,19 +166,29 @@ namespace DMM.Pages
             this.txt_totalDebit.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txt_totalDebit.BackColor = System.Drawing.Color.Purple;
             this.txt_totalDebit.ForeColor = System.Drawing.Color.White;
-            this.txt_totalDebit.Location = new System.Drawing.Point(-24, 115);
+            this.txt_totalDebit.Location = new System.Drawing.Point(3, 115);
             this.txt_totalDebit.Name = "txt_totalDebit";
-            this.txt_totalDebit.Size = new System.Drawing.Size(370, 69);
+            this.txt_totalDebit.Size = new System.Drawing.Size(300, 69);
             this.txt_totalDebit.TabIndex = 0;
             this.txt_totalDebit.Text = "1 000 000";
             this.txt_totalDebit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // chartControl1
             // 
-            this.chartControl1.Location = new System.Drawing.Point(18, 232);
+            this.chartControl1.DataSource = typeof(DMM.Debit_Customers);
+            xyDiagram1.AxisX.DateTimeScaleOptions.AggregateFunction = DevExpress.XtraCharts.AggregateFunction.Sum;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Location = new System.Drawing.Point(30, 244);
+            this.chartControl1.Margin = new System.Windows.Forms.Padding(15);
             this.chartControl1.Name = "chartControl1";
-            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
-            this.chartControl1.Size = new System.Drawing.Size(300, 200);
+            series1.ArgumentDataMember = "DateT";
+            series1.Name = "Series 1";
+            series1.ValueDataMembersSerializable = "Debit";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.Size = new System.Drawing.Size(960, 200);
             this.chartControl1.TabIndex = 3;
             // 
             // AnalysisPage
@@ -185,11 +199,13 @@ namespace DMM.Pages
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "AnalysisPage";
-            this.Size = new System.Drawing.Size(1151, 463);
+            this.Size = new System.Drawing.Size(1031, 463);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             this.ResumeLayout(false);
 
